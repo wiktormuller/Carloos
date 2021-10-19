e9b0b928c6d8a454371de210fe5a06c16a9b7929
wiktormuller <biznes.wiktor@gmail.com>
2021-10-19 18:30:23 +0200

    New structure of backend

--- src/backend/JobJetRestApi/JobJetRestApi.Web/Startup.cs
+++ src/backend/JobJetRestApi/JobJetRestApi.Web/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace JobJetRestApi
+namespace JobJetRestApi.Web
 {
     public class Startup
     {
@@ -29,7 +29,7 @@ namespace JobJetRestApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "JobJetRestApi", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "JobJetRestApi.Web", Version = "v1"});
             });
         }
 
@@ -40,7 +40,7 @@ namespace JobJetRestApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobJetRestApi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobJetRestApi.Web v1"));
             }
 
             app.UseHttpsRedirection();
