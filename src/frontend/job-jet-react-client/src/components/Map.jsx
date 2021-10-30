5f362268dcbd493b38d2d39f7951af1c460a2957
Maciej Paluszynski <macpalus@gmail.com>
2021-10-30 21:18:15 +0200

    implementing temporary makeshift map

--- src/frontend/job-jet-react-client/src/components/Map.jsx
+++ src/frontend/job-jet-react-client/src/components/Map.jsx
@@ -3,6 +3,7 @@ import '../styles/main-styles.css';
 export const Map = () => {
     return (
       <div className="map">
+        <img src="https://i.stack.imgur.com/RdkOb.jpg" alt="blablabla"  width="99.5%" height="99.5%"></img>
       </div>
       );
 }
