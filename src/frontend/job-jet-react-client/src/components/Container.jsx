1270a5d9f59252cacf42dea018e2c2af2a068514
kamugo <74856061+kamugo@users.noreply.github.com>
2021-11-09 20:00:43 +0100

    Class converted to component

--- src/frontend/job-jet-react-client/src/components/Container.jsx
+++ src/frontend/job-jet-react-client/src/components/Container.jsx
@@ -1,7 +1,7 @@
 import '../styles/main-styles.css';
 import { Header } from "./Header.jsx";
 import { Data } from  "./Data.jsx";
-import  Map  from  "./Map.jsx";
+import { Map } from  "./Map.jsx";
 
 
 
--- src/frontend/job-jet-react-client/src/components/Map.jsx
+++ src/frontend/job-jet-react-client/src/components/Map.jsx
@@ -1,74 +1,37 @@
-import React, {Component} from 'react';
+import '../styles/main-styles.css';
+import React from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import {adsArray} from '../data/arrays'
-import { Data } from "./Data"
 
-
-
-
-
-class Map extends Component {
-  states = {
-    center:{
-      lat: 52.006376,
-      lng: 19.025167,
-    },
-    zoom: 6.8,
-    layer:[]
-  }
- 
+export const Map = () =>{
+  const center = [52.006376,19.025167] ;
+  const zoom = 6.8;
+  var skill = 0;
   
- 
-  render(){
-    
-    return (
-      
-      <MapContainer className="map" center={this.states.center} zoom={this.states.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-         {adsArray.map(job => (
-           this.skill=job.skills[0],
-           
-        <Marker
-          icon={L.icon({
-            iconUrl: require(`../data/icons/`+this.skill+`.svg`).default,
-            iconSize: new L.Point(60, 75),
-          })}
-          key={job.id}
-          position={[
+  return (
+    <MapContainer className="map" center={center} zoom={zoom} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-            job.lat,
-            job.lng
-          ]} 
-           >
-            <Popup
-          position={[
-            job.lat,
-            job.lng
-          ]}
-          
-        >
-          <div>
-            <h2>{job.title}</h2>
-            <p>{job.description}</p>
-          </div>
-        </Popup>
-            </Marker>
-            
-        
-        
-        
+      {adsArray.map(job => (
+        skill=job.skills[0],
+        <Marker icon={L.icon({
+          iconUrl: require(`../data/icons/`+skill+`.svg`).default,
+          iconSize: new L.Point(60, 75),
+        })}
+        key = {job.id} position={[job.lat, job.lng]}>
+          <Popup position={[job.lat, job.lng]}>
+            <div>
+              <h2>{job.title}</h2>
+              <p>{job.description}</p>
+            </div>
+          </Popup>
+        </Marker>
       ))}
-     
-      </MapContainer>
-      
-    );
-  }
-  
+    </MapContainer>
+  );
 }
-
-export default Map;
