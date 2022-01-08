4e5550af4a1b9c41b7863bc136461d25ca1bc624
kamugo <74856061+kamugo@users.noreply.github.com>
2022-01-08 18:06:42 +0100

    Update Map.jsx

--- src/frontend/job-jet-react-client/src/components/map/Map.jsx
+++ src/frontend/job-jet-react-client/src/components/map/Map.jsx
@@ -14,6 +14,37 @@ export const Map = (props) => {
   //informacje o geolokacji miejsca pracy:
   console.log(props.advertLocation);
 
+  const url = 'https://jobjet.azurewebsites.net/api/v1/roads/17.9666195%2C54.1228639%3B17.968999%2C54.118802';
+  /*function myfunction(x)
+  {road = x.map(
+    (r)=>(
+      <Marker 
+      position ={[r.longitude,r.latitude]}
+      ></Marker>
+    )
+  );}*/
+
+ /* async function getRandomUser() {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  let datas=getRandomUser();
+  console.log(datas)*/
+//   let positionArray=fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     return data[0];
+// })
+// console.log(positionArray)
+let obj;
+let Array=fetch(url)
+  .then(res => res.json())
+  .then(data => obj = data)
+  .then(() => { return obj })
+console.log(Array)
+
+
   const filteredLocalization = props.localizationArray.filter(
     (loc) => loc.id === 1
   );
@@ -44,6 +75,15 @@ export const Map = (props) => {
     )
   );
 
+  const arr = [{ longitude: 17.96664, latitude: 54.122832 }, { longitude: 17.967721, latitude: 54.123033 }, { longitude: 17.96799, latitude: 54.123077 }]
+  const renderedArray = arr.map((obj) => {
+    return (
+      <Marker
+        position={[obj.latitude, obj.longitude]}
+      />
+    );
+  });
+
   return (
     <MapContainer
       className="map"
@@ -56,6 +96,9 @@ export const Map = (props) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {filteredAdverts}
+      {renderedArray}
+
+
     </MapContainer>
   );
 };
