22f4361886986ff9349e90ad143a32c29c8f8ec3
Wiktor Muller <biznes.wiktor@gmail.com>
2022-11-28 22:31:54 +0100

    feat(API): Add query to get user profile information

---
+++ src/backend/JobJetRestApi/JobJetRestApi.Application/Exceptions/ProfileNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace JobJetRestApi.Application.Exceptions;
+
+public class ProfileNotFoundException: System.Exception
+{
+    private ProfileNotFoundException(string message) : base(message) {}
+
+    public static ProfileNotFoundException ForId(int id) =>
+        new ProfileNotFoundException($"Profile with Id: #{id} not found.'");
+}
