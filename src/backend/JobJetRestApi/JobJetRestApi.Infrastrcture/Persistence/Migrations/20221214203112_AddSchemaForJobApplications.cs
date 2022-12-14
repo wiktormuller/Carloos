6959b6a3f9237e83abfcaa6fb32b9994a6bcc020
Wiktor Muller <biznes.wiktor@gmail.com>
2022-12-14 22:49:45 +0100

    feat(job-offers-application-backend): Implement repository and infrastructure part of the job applications

---
+++ src/backend/JobJetRestApi/JobJetRestApi.Infrastrcture/Persistence/Migrations/20221214203112_AddSchemaForJobApplications.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobJetRestApi.Infrastructure.Persistence.Migrations
+{
+    public partial class AddSchemaForJobApplications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobOfferApplications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileExtension = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileBytes = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    JobOfferId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobOfferApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobOfferApplications_JobOffers_JobOfferId",
+                        column: x => x.JobOfferId,
+                        principalTable: "JobOffers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobOfferApplications_JobOfferId",
+                table: "JobOfferApplications",
+                column: "JobOfferId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobOfferApplications");
+        }
+    }
+}
---
+++ src/backend/JobJetRestApi/JobJetRestApi.Infrastructure/Repositories/JobOfferApplicationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using JobJetRestApi.Domain.Entities;
+using JobJetRestApi.Domain.Repositories;
+using JobJetRestApi.Infrastructure.Persistence.DbContexts;
+
+namespace JobJetRestApi.Infrastructure.Repositories;
+
+public class JobOfferApplicationRepository : IJobOfferApplicationRepository
+{
+    private readonly JobJetDbContext _jobJetDbContext;
+    
+    public JobOfferApplicationRepository(JobJetDbContext jobJetDbContext)
+    {
+        _jobJetDbContext = Guard.Against.Null(jobJetDbContext, nameof(jobJetDbContext));
+    }
+    
+    public async Task CreateAsync(JobOfferApplication jobOfferApplication)
+    {
+        _jobJetDbContext.JobOfferApplications.Add(jobOfferApplication);
+        await _jobJetDbContext.SaveChangesAsync();
+    }
+}
