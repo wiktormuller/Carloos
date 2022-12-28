1b2af847dce1d8c2de164fa0a8a9be8db583abf0
Wiktor Muller <biznes.wiktor@gmail.com>
2022-12-28 21:54:08 +0100

    feat(activate-and-reset-UI): Add mock pages for activating account and reseting password

---
+++ src/frontend/job-jet-react-client/src/pages/resetPassword/components/ResetPassword.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../reset-password.css';
+
+export default function ResetPasswordComponent()
+{
+    return (
+        <div>
+            <p>Reset Password</p>
+        </div>
+    );
+}
