09694faa041220eb2697a8ca8b82ad60d1a16a01
Wiktor Muller <biznes.wiktor@gmail.com>
2022-12-31 12:22:27 +0100

    feat(UI): Add pages for pageNotFound and acceddDenied

--- src/frontend/job-jet-react-client/src/pages/accessDenied/components/AccessDeniedComponent.jsx
+++ src/frontend/job-jet-react-client/src/pages/accessDenied/components/AccessDeniedComponent.jsx
@@ -1,8 +1,20 @@
 import "../accessDenied-styles.css";
+import { Link } from 'react-router-dom';
 
 export default function AccessDeniedComponent()
 {
   return (
-    <h1>Access Denied for this page!</h1>
+    <div class="accessDenied">
+            <div class="d-flex align-items-center justify-content-center vh-100">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold">403</h1>
+                    <p class="fs-3"> <span class="text-danger">Opps!</span> Forbidden.</p>
+                    <p class="lead">
+                        Access Denied!
+                    </p>
+                    <Link className="btn btn-primary" to={`/`}>Go home</Link>
+                </div>
+            </div>
+        </div>
   );
 }
